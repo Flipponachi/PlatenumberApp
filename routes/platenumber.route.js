4717235acf23fee625a4fbca14c7c240681c070d
Flipponachi <iamflipponachi@gmail.com>
2018-12-22 12:43:54 +0100

    Completed implementation to create new plates

--- routes/platenumber.route.js
+++ routes/platenumber.route.js
@@ -14,7 +14,6 @@ plateNumberRouter.get('/', (req, res) => {
 plateNumberRouter.get('/createplate', (req, res) => {
 
     LgaModel.find({}, (err, data) => {
-        console.log(data);
         res.render('createplate', {lgas: data})
     })
     
@@ -22,18 +21,97 @@ plateNumberRouter.get('/createplate', (req, res) => {
 
 //Post: Create a platenumber
 plateNumberRouter.post('/createplate', (req, res) => {
-  var model = new PlateNumberModel({
-    name: req.body.lga.name
-  });
 
-  model.save()
-  .then(() => {
-    res.redirect('/lga')
-  }
-  
-  )
-  .catch((err) => {
-    res.redirect('/lga/createlga')})
+
+//Find the LGA and cretae abbreviation
+LgaModel.findById(req.body.plates.lgaId)
+.then((data) => {
+    var lgaName = data.name;
+    var plateDetails = [data._id]
+
+    if(lgaName.length > 3){
+        var abbrLga = (lgaName.charAt(0) + lgaName.charAt(lgaName.length - 2) + lgaName.charAt(lgaName.length -1)).toUpperCase();
+        plateDetails.push(abbrLga);
+        return plateDetails
+    }
+    plateDetails.push(lgaName);
+    return plateDetails;
+})
+.then((plateDetails) => {
+
+    
+       //Check if any record in the data base first for the platenumber
+        PlateNumberModel.findOne({}).sort({creationTime: -1})
+        .then((data) => {
+           
+            //Amount of plates requested by user
+            var noOfPlates = req.body.plates.amount;
+
+            //create new plate number if no records
+            if(data === null){           
+               //Loop through and create plates
+                for(var i = 1; i <= noOfPlates; i++){
+                    
+                    var newPlatenumber = new PlateNumberModel({
+                        number: plateDetails[1]+"00"+i+"AA",
+                        lga: plateDetails[0]
+                     });
+                     newPlatenumber.save();
+                }
+                
+            }
+            //Else split last record get the last number
+            var lastNumber = data.number.slice(3, -2);
+            
+            //Check if limit is reached
+            var parsedLastNumber = parseInt(lastNumber, 10);
+
+            if(parsedLastNumber === 999){
+               //Reset last letter and last number
+            }
+
+            //create plates
+
+            //Loop through and create plates
+            for(var i = 1; i <= noOfPlates; i++){
+                 var amount = (parsedLastNumber+i).toString();                              
+                                 
+                 var newPlatesNumber = [];
+                 if(amount.length === 1){
+                     newPlatesNumber.push("00"+amount);
+                 }
+                 if(amount.length == 2){
+                     newPlatesNumber.push("0"+amount);
+                 }
+                 if(amount.length === 3){
+                     newPlatesNumber.push(amount);
+                 }
+
+                 //Create new record
+                 newPlatesNumber.forEach((num) => {
+                      var newPlatenumber = new PlateNumberModel({
+                           number: plateDetails[1]+num+"AA",
+                           lga: plateDetails[0]
+                        });
+                 newPlatenumber.save();
+                 })
+               
+            }
+            
+            
+
+        })
+        .catch((err) => {
+            
+        })
+
+
+})
+.catch((err) => { 
+    console.log(err)
+});
+//Check if any entry in the platenumber table, if no create new entry based on the user input
+res.redirect('/lga');
 })
 
 module.exports = plateNumberRouter;
